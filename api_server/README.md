3fd2afdeb9110e4dc35aa4aa9508e02621b43202
root <root@at2.example.net>
2020-08-13 19:11:54 -0400

    Test Callback Plugin

---
+++ api_server/README.md
@@ -0,0 +1,26 @@
+# mock-server for callback plugin testing
+
+## Configure the server
+
+Using the server\_config.ini file
+
+### configuration parameters
+host = the ip address the server should serve from - default: 0.0.0.0
+port = the port the server should server from - default: 5000
+user = the username for basic auth - default: admin
+password = the password for basic auth - default: password
+
+## To run the server
+
+From repository root:
+
+* `python3 -m venv env` or `virtualenv env` (for python2.x)
+* `source env/bin/activate`
+* `pip install -r requirements.txt`
+* optionally configure api server defaults in server\_config.ini
+* `make run`
+
+### available functions
+
+* post using `make post` (requires auth)
+* list using `make list`
---
+++ callback_plugins/test_callback_plugin.py
@@ -0,0 +1,85 @@
+# written for python2.7
+# Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+from ansible.plugins.callback import CallbackBase
+from ansible import constants as C
+from ansible.utils.display import Display
+from __main__ import cli
+from ansible.utils.color import colorize, hostcolor
+
+
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
+DOCUMENTATION = '''
+    callback: example_callback_plugin
+    type: notification
+    short_description: Send callback on various runners to an API endpoint.
+    description:
+      - On ansible runner calls report state and task output to an API endpoint.
+      - Configuration via callback_config.ini, place the file in the same directory
+        as the plugin.
+    requirements:
+      - python requests library
+      - HTTPBasicAuth library from python requests.auth
+      - ConfigParser for reading configuration file
+    '''
+
+class CallbackModule(CallbackBase):
+
+    '''
+    Callback to API endpoints on ansible runner calls.
+    '''
+
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'notification'
+    CALLBACK_NAME = 'example_callback_plugin'
+
+    def __init__(self, *args, **kwargs):
+        super(CallbackModule, self).__init__()
+
+    def v2_playbook_on_start(self, playbook):
+        self.playbook = playbook
+        print(playbook.__dict__)
+
+    def v2_playbook_on_play_start(self, play):
+        self.play = play
+        self.extra_vars = self.play.get_variable_manager().extra_vars
+        self.callback_url = self.extra_vars['callback_url']
+        self.username = self.extra_vars['username']
+        self.password = self.extra_vars['password']
+
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        payload = {'state': result._host.name,
+                   'task_name': result.task_name,
+                   'task_output' : result._result['msg']
+                  }
+
+        requests.post(self.callback_url,auth=(self.username,self.password),data=payload).json()
+        pass
+
+
+    def v2_playbook_on_stats(self, stats):
+        hosts = sorted(stats.processed.keys())
+        hostDict = {}
+        for h in hosts:
+            t = stats.summarize(h)
+            if t['failures'] > 0:
+                hostDict[h] = 'FAILED'
+            elif t['unreachable'] > 0:
+                hostDict[h] = 'UNREACHABLE'
+            else: 
+                hostDict[h] = 'SUCCEEDED'
+        hostDict = json.dumps(hostDict)
+        payload = {'state': hostDict,
+                   }
+        requests.post(self.callback_url,auth=(self.username,self.password),data=payload).json()
+        pass
